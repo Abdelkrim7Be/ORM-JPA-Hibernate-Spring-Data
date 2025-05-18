b5465783f5ab76d8cc0334cc5f89ef6caa21227c
Abdelkrim7Be <119794076+Abdelkrim7Be@users.noreply.github.com>
2025-05-18 06:29:31 +0100

    Created Patient JPA entity with required fields and Lombok annotations

---
+++ src/main/java/com/example/patientmanagement/entities/Patient.java
@@ -0,0 +1,25 @@
+package com.example.patientmanagement.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private Date dateNaissance;
+    private boolean malade;
+    private int score;
+}
